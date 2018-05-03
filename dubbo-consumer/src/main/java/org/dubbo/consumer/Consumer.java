3134ebf2c53fa50c989e8fffd55b0fcfd3e0097a
frank-neo <lishaoxiong@lvmama.com>
2018-05-03 16:44:15 +0800

    Dubbo生产者消费者实例

---
+++ dubbo-consumer/src/main/java/org/dubbo/consumer/Consumer.java
@@ -0,0 +1,18 @@
+package org.dubbo.consumer;
+
+import org.dubbo.api.DemoService;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Consumer {
+    public static void main(String[] args) {
+
+        //测试常规服务
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("consumer.xml");
+        context.start();
+        System.out.println("consumer start");
+        DemoService demoService = (DemoService) context.getBean(DemoService.class);
+        System.out.println("consumer");
+        System.out.println(demoService.getPermissions(1L));
+    }
+}
